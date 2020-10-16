e42d0a0a3bac160a944bbfcb6801881ff56412dd
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-10-16 15:02:36 +0200

    typo

--- games/dash/logic/blocks/Wall.js
+++ games/dash/logic/blocks/Wall.js
@@ -1,7 +1,7 @@
 import Block from '@/games/dash/logic/blocks/Block'
 
 export default class Wall extends Block {
-  constructor(positon) {
-    super(false, false, positon)
+  constructor(position) {
+    super(false, false, position)
   }
 }
