e3b674d97c930a9b7c0b9b7628f1e0316e51dc19
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2022-04-06 19:50:14 +0200

    Add ugly code execution proof of concept.

--- components/BlocklyComponent.vue
+++ components/BlocklyComponent.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
     <v-row justify="center" no-gutters>
-      <v-btn small outlined class="my-1 mx-2 font-weight-black" color="info">
+      <v-btn
+        small
+        outlined
+        class="my-1 mx-2 font-weight-black"
+        color="info"
+        @click="updateCurrentCode"
+      >
         <v-icon>save</v-icon>
       </v-btn>
-      <v-btn small outlined class="my-1 mx-2 font-weight-black" color="success">
+      <v-btn
+        small
+        outlined
+        class="my-1 mx-2 font-weight-black"
+        color="success"
+        @click="runCode"
+      >
         <v-icon>play_arrow</v-icon>
       </v-btn>
       <v-btn small outlined class="my-1 mx-2 font-weight-black" color="error">
@@ -26,6 +38,7 @@
 import Blockly from 'blockly'
 import BlocklyJS from 'blockly/javascript'
 import * as Hu from 'blockly/msg/hu'
+import Interpreter  from 'js-interpreter';
 
 export default {
   name: 'BlocklyComponent',
@@ -135,7 +148,6 @@ export default {
     }
 
     Blockly.Blocks.turnleft = {
-      // eslint-disable-next-line
       init: function () {
         this.appendDummyInput()
           .setAlign(Blockly.ALIGN_RIGHT)
@@ -155,18 +167,236 @@ export default {
   },
 
   methods: {
+    run(){
+      this.isCancel = false;
+      let highlightBlock= (id)=>{
+          this.workspace.highlightBlock(id);
+      }
+      let StepForward=()=>{
+          move =true;
+          // this.$nuxt.$emit('STEP_FORWARD',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          console.log("player.stepForward()");
+      }
+      let StepBack=()=>{
+          move =true;
+          // this.$nuxt.$emit('STEP_BACK',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          console.log("player.stepBack()");
+      }
+      let TurnLeft=()=>{
+          move =true;
+          // this.$nuxt.$emit('TURN_LEFT',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          console.log("player.rotLeft()");
+      }
+      let TurnRight=()=>{
+          // move =true;
+          // this.$nuxt.$emit('TURN_RIGHT',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });/my
+          console.log("player.rotRight()");
+      }
+      var initApi = function(myInterpreter, scope){
+        let wrapper;
+          wrapper = function() {
+              return StepForward();
+          };
+          myInterpreter.setProperty(scope, 'StepForward',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function() {
+              return StepBack();
+          };
+          myInterpreter.setProperty(scope, 'StepBack',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function() {
+              return TurnLeft();
+          };
+          myInterpreter.setProperty(scope, 'TurnLeft',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function() {
+              return TurnRight();
+          };
+          myInterpreter.setProperty(scope, 'TurnRight',
+          myInterpreter.createNativeFunction(wrapper));
+
+          //highlightBlock
+          wrapper = function(text) {
+              return highlightBlock(text);
+          };
+          myInterpreter.setProperty(scope, 'highlightBlock',
+          myInterpreter.createNativeFunction(wrapper));
+
+      }
+      //console.log(this.code);
+      var myCode = this.code;
+      var myInterpreter = new Interpreter(myCode, initApi)
+
+      let move =false;
+
+      // let db = 1000000;
+      let db = 10000;
+      let uresJaras = ()=>{
+          try{
+              while(myInterpreter.step() && !move && !this.isCancel && db > 0){
+                  db--;
+                  if(db == 0){
+                      alert("Végtelen ciklust, vagy túlsok számolást csináltál! :)")
+                  }
+              }
+          }catch(e){
+              alert(e);
+              this.isCancel = true;
+          }
+      };
+      uresJaras();
+    },
+    runCode() {
+      var highlightBlock= (id)=>{
+          this.workspace.highlightBlock(id);
+      }
+      let StepForward=(id)=>{
+          move =true;
+          // this.$nuxt.$emit('STEP_FORWARD',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          this.highlightBlock(id);
+          console.log("player.stepForward()");
+      }
+      let StepBack=(id)=>{
+          move =true;
+          // this.$nuxt.$emit('STEP_BACK',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          this.highlightBlock(id);
+          console.log("player.stepBack()");
+      }
+      let TurnLeft=(id)=>{
+          move =true;
+          // this.$nuxt.$emit('TURN_LEFT',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });
+          this.highlightBlock(id);
+          console.log("player.rotLeft()");
+      }
+      let TurnRight=(id)=>{
+          // move =true;
+          // this.$nuxt.$emit('TURN_RIGHT',()=>{
+          //     move =false;
+          //     uresJaras();
+          // });/my
+          this.highlightBlock(id);
+          console.log("player.rotRight()");
+      }
+      var initApi = function(myInterpreter, scope){
+        let wrapper;
+          wrapper = function(id) {
+              return StepForward(id);
+          };
+          myInterpreter.setProperty(scope, 'StepForward',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function(id) {
+              return StepBack(id);
+          };
+          myInterpreter.setProperty(scope, 'StepBack',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function(id) {
+              return TurnLeft(id);
+          };
+          myInterpreter.setProperty(scope, 'TurnLeft',
+          myInterpreter.createNativeFunction(wrapper));
+
+          wrapper = function(id) {
+              return TurnRight(id);
+          };
+          myInterpreter.setProperty(scope, 'TurnRight',
+          myInterpreter.createNativeFunction(wrapper));
+
+          //highlightBlock
+          wrapper = function(text) {
+              return highlightBlock(text);
+          };
+          myInterpreter.setProperty(scope, 'highlightBlock',
+          myInterpreter.createNativeFunction(wrapper));
+
+      }
+      let myCode = this.$store.state.code.currentCode;
+      let move =false;
+      var myInterpreter = new Interpreter(myCode, initApi)
+      console.log(myCode);
+
+        // First statement of this code.
+        // Clear the program output.
+        // runButton.disabled = 'disabled';
+
+        // And then show generated code in an alert.
+        // In a timeout to allow the outputArea.value to reset first.
+        setTimeout(function() {
+          alert('Ready to execute the following code\n' +
+            '===================================\n' +
+            myCode);
+
+          // Begin execution
+          let runner = function() {
+            if (myInterpreter) {
+              var hasMore = myInterpreter.step();
+              if (hasMore) {
+                // Execution is currently blocked by some async call.
+                // Try again later.
+                setTimeout(runner, 10);
+              } else {
+                // Program is complete.
+                // resetInterpreter();
+              }
+            }
+          };
+          runner();
+        }, 1);
+        return;
+    },
+    highlightBlock(id) {
+      this.workspace.highlightBlock(id);
+    },
+    resetInterpreter() {
+      myInterpreter = null;
+      if (runner) {
+        clearTimeout(runner);
+        runner = null;
+      }
+    },
     showCode() {
       this.code = this.$store.state.code.currentCode
+      // eslint-disable-next-line no-console
+      console.log(this.code)
     },
     doing() {
       // eslint-disable-next-line no-console
       console.log('yay')
     },
     updateCurrentCode() {
+      // Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
+      // Blockly.JavaScript.addReservedWords('highlightBlock');
       const c = BlocklyJS.workspaceToCode(this.workspace)
       // this.$store.dispatch('code/updateCurrentCode', c)
       this.$store.commit('code/updateCurrentCode', c)
-      this.$emit('update', c)
+    },
+    playCode() {
+      // eslint-disable-next-line no-console
     },
   },
 }
@@ -176,7 +406,6 @@ export default {
 .blocklyDiv {
   height: 100%;
   width: 100%;
-  text-align: left;
 }
 #blockly2 {
   position: absolute;
